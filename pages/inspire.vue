fba678b21a524f8fc35e76dc477e99fe4f0233ea
h-teshima <tesukonotessy@gmail.com>
2019-11-06 02:40:08 +0000

    click center

--- pages/inspire.vue
+++ pages/inspire.vue
@@ -2,7 +2,7 @@
 <v-layout row justify-center align-center>
 
 <GmapMap
-  :center="{lat:34.8, lng:135.2}"
+  :center="center"
   :zoom="9"
   map-type-id="terrain"
   style="width: 500px; height: 300px"
@@ -27,7 +27,8 @@ export default {
       markers: [
         {position: { lng: 134.8, lat: 34.9 }},
         {position: { lng: 135.6, lat: 34.9 }}
-      ]
+      ],
+      center: {lat:34.8, lng:135.2}
     };
   }
 };
